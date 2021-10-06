1fa125fb5c471b249e3b2bfd71a37a0ab0b13229
Alyssa Harris <ah00214@my.westga.edu>
2021-10-06 10:40:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
-function makePerson(first, last, age, favColor) {
-  return {
-    first,
-    last,
-    age,
-    favColor,
-    isAlive: true,
-  };
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk() {
+    return "Beep.";
+  }
+
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+  }
+}
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+console.log(myFirstVehicle.honk());
+console.log(myFirstVehicle.toString());
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+
+  revEngine() {
+    return "VROOM!!";
+  }
+}
+
+class Garage {
+  constructor(vehicles, capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
 }
